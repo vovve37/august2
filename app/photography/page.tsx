4b863dc995c846137df6c17b51142f01e3f65fd9
vovve37 <hello@august.se>
2025-10-31 09:23:58 +0100

    Add new images to PhotoPage component

--- app/photography/page.tsx
+++ app/photography/page.tsx
@@ -3,11 +3,27 @@ import GridWithLightbox from "@/components/GridWithLightbox";
 export default function PhotoPage(){
   const images = [
     { src: "/images/photography_01.jpg", title: "H&M" },
+    { src: "/images/photography_02.jpg", title: "H&M" },
     { src: "/video/photography_03.mov", title: "H&M" },
+    { src: "/images/photography_04.jpg", title: "H&M" },
+    { src: "/images/photography_05.jpg", title: "Sköna Hem" },
+    { src: "/images/photography_06.jpg", title: "Sköna Hem" },
+    { src: "/images/photography_07.jpg", title: "H&M" },
     { src: "/images/photography_08.jpg", title: "H&M" },
+    { src: "/images/photography_09.jpg", title: "H&M" },
+    { src: "/images/photography_10.jpg", title: "H&M" },
+    { src: "/images/photography_11.jpg", title: "H&M" },
+    { src: "/images/photography_12.jpg", title: "H&M" },
+    { src: "/images/photography_13.jpg", title: "H&M" },
     { src: "/images/photography_14.jpg", title: "H&M" },
-    { src: "/images/photography_21.jpg", title: "Elle Deco" },
     { src: "/images/photography_15.jpg", title: "Sköna Hem" },
+    { src: "/images/photography_16.jpg", title: "Stockholm Fashion Week" },
+    { src: "/images/photography_17.jpg", title: "Skanska" },
+    { src: "/images/photography_18.jpg", title: "H&M" },
+    { src: "/images/photography_19.jpg", title: "H&M" },
+    { src: "/images/photography_20.jpg", title: "H&M" },
+    { src: "/images/photography_21.jpg", title: "Elle Deco" },
+    { src: "/images/photography_22.jpg", title: "Sköna Hem" },
   ];
 
   return (
